9c4ec1e87719499f5b8f38cb09c1ce723e63d08f
Thibaud Mouche <tmouche@z3r4p6.42lyon.fr>
2024-05-24 19:29:04 +0200

    some leaks removed

--- exec/SRCS/exec_open.c
+++ exec/SRCS/exec_open.c
@@ -6,7 +6,7 @@
 /*   By: tmouche <tmouche@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 18:12:53 by tmouche           #+#    #+#             */
-/*   Updated: 2024/05/24 19:05:05 by tmouche          ###   ########.fr       */
+/*   Updated: 2024/05/24 19:27:47 by tmouche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,17 +56,24 @@ static int	_heredoc_handling(t_data *args, t_file *file)
 	{
 		line = get_next_line(STDIN_FILENO);
 		if (!line)
+		{
+			free (line);
+			close (pipe_heredoc[0]);
+			close (pipe_heredoc[1]);
 			_exit_failure(args);
+		}
 		if (file->name && ft_strncmp(line, file->name[1], name_len + 1) == 0)
 			break ;
 		line = _pars_heredoc(args, file, line);
 		if (write(pipe_heredoc[1], line, ft_strlen(line, 0)) == -1)
 		{
+			free (line);
 			close (pipe_heredoc[0]);
 			close (pipe_heredoc[1]);
 			_exit_failure(args);
 		}
 	}
+	free (line);
 	close (pipe_heredoc[1]);
 	return (pipe_heredoc[0]);
 }
